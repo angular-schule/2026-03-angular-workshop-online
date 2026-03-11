93a35600e9829ca837807bc2bd93b51741b4ff4e
Ferdinand Malcher <ferdinand@malcher.media>
2026-03-11 11:37:02 +0100

    DashboardPage generieren und einbinden

--- book-rating/src/app/app.ts
+++ book-rating/src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardPage } from './books/dashboard-page/dashboard-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardPage],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Book Rating');
 }
+
+
+
+/* TODO
+- Datenmodell
+- (Bücher laden)
+- Daten (heute per Hand definiert)
+- Komponente für die "Seite": DashboardPage
+- Signal mit Array von Büchern
+- Komponente für einzelnes Buch: BookCard
+
+*/
