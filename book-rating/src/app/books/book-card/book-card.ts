d570fb178b486f1116ef73709867ee755735eb2f
Ferdinand Malcher <ferdinand@malcher.media>
2026-03-11 12:21:09 +0100

    BookCard, Inputs, Property Bindings

---
+++ book-rating/src/app/books/book-card/book-card.ts
@@ -0,0 +1,14 @@
+import { Component, input } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'app-book-card',
+  imports: [],
+  templateUrl: './book-card.html',
+  styleUrl: './book-card.scss',
+})
+export class BookCard {
+  // Input: hier fließen Daten von der Elternkomponente hinein
+  // von oben nach unten
+  readonly book = input.required<Book>();
+}
--- book-rating/src/app/books/dashboard-page/dashboard-page.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Book } from '../shared/book';
+import { BookCard } from "../book-card/book-card";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [BookCard],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
 })
-export class DashboardPage {}
+export class DashboardPage {
+  protected readonly books = signal<Book[]>([]);
+
+  constructor() {
+    this.books.set([
+      {
+        isbn: '123',
+        title: 'Angular',
+        description: 'Grundlagen und mehr',
+        price: 42.9,
+        rating: 5,
+        authors: ['FM', 'DK', 'JH']
+      },
+      {
+        isbn: '456',
+        title: 'Vue.js',
+        description: 'Das grüne Framework',
+        price: 46.9,
+        rating: 3,
+        authors: ['FD']
+      }
+    ])
+  }
+}
