f128b6f0d436b8fcffb985dda74bf1ce1ae8d684
Ferdinand Malcher <ferdinand@malcher.media>
2026-03-12 12:09:09 +0100

    app.spec.ts: toContain => toBe

--- book-rating/src/app/app.spec.ts
+++ book-rating/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Book Rating');
+    expect(compiled.querySelector('h1')?.textContent).toBe('Book Rating');
   });
 });
