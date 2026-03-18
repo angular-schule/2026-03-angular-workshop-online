e8045dc310c4332fa06d6e111f779b4981e8fe89
Ferdinand Malcher <ferdinand@malcher.media>
2026-03-18 11:30:45 +0100

    Detailseite mit RxJS und toSignal()

--- book-rating/src/app/books/book-details-page/book-details-page.ts
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -1,14 +1,23 @@
 import { Component, inject, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { BookStore } from '../shared/book-store';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-details-page',
-  imports: [RouterLink],
+  imports: [RouterLink, JsonPipe],
   templateUrl: './book-details-page.html',
   styleUrl: './book-details-page.scss',
 })
 export class BookDetailsPage {
   readonly isbn = input.required<string>();
   protected readonly book = inject(BookStore).getSingleResource(this.isbn);
+
+  #store = inject(BookStore);
+
+  protected readonly book2 = toSignal(toObservable(this.isbn).pipe(
+    switchMap(isbn => this.#store.getSingle(isbn))
+  ));
 }
