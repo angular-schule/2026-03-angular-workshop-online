40d1efc6c9036e0b7f3aadb4ba7eae6affd1b290
Ferdinand Malcher <ferdinand@malcher.media>
2026-03-18 14:00:48 +0100

    Typeahead-Suche

--- book-rating/src/app/books/book-search-page/book-search-page.ts
+++ book-rating/src/app/books/book-search-page/book-search-page.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { form, FormField } from '@angular/forms/signals';
+import { debounceTime, distinctUntilChanged, filter, map, of, switchMap } from 'rxjs';
+import { BookStore } from '../shared/book-store';
 
 @Component({
   selector: 'app-book-search-page',
-  imports: [],
+  imports: [FormField],
   templateUrl: './book-search-page.html',
   styleUrl: './book-search-page.scss',
 })
-export class BookSearchPage {}
+export class BookSearchPage {
+  #store = inject(BookStore);
+
+  protected readonly searchTerm = signal('');
+  protected readonly searchForm = form(this.searchTerm);
+
+  protected readonly results = toSignal(toObservable(this.searchTerm).pipe(
+    debounceTime(500),
+    // filter(term => term.length >= 3),
+    distinctUntilChanged(),
+    switchMap(term => {
+      if (term.length >= 3) {
+        return this.#store.search(term)
+      } else {
+        return of([]);
+      }
+    })
+  ), { initialValue: [] })
+}
